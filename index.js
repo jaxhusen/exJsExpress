4741e0a56fc3a07d657ae7dc58efd9ab97d6329e
jaxhusen <jaxhusen@hotmail.com>
2023-01-06 11:36:23 +0100

    sqlite3 db klar

--- index.js
+++ index.js
@@ -1,11 +1,108 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 var orders = require('./orders.json'); // json file path
 
-/* console.log(orders); */
+
+// https://www.youtube.com/watch?v=xDYx5UdHwv0
+const sqlite3 = require('sqlite3').verbose();
+
+ const db = new sqlite3.Database('./users.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+
+    console.log("connection successful")
+}); 
 
 
-app.get("/orders", function(req, res, next) {
+
+const sql = `SELECT * FROM users`;
+
+
+    db.all(sql, [], (err,rows) => {
+        if(err) return console.error(err.message);
+
+        rows.forEach((row) => {
+            console.log(row)
+        })
+    })  
+
+
+ db.close((err) => {
+    if (err) return console.error(err.message);
+}) 
+
+
+
+
+
+
+
+
+/* ---------   KOD FÖR ATT SKAPA NYA RADER FÖR USERS HÄMTAT FRÅN ETT API   ------------
+
+ axios
+    .get("http://random-data-api.com/api/users/random_user")
+    .then((response) => {
+        const { data } = response;
+        const { username} = data;
+        const { password } = data;
+        const { id } = data;
+
+
+      const sql = `INSERT INTO users(username, password, id)
+                    VALUES (?, ?, ?)`; 
+
+    db.run(sql, [username, password, id], (err) => {
+        if(err) return console.error(err.message);
+        console.log("a new row has been created")
+    })  
+
+
+    db.close((err) => {
+        if (err) return console.error(err.message);
+    })
+
+
+    }).catch((error) => {
+        if (error) return console.error(error);
+    }); 
+
+
+ */
+
+
+
+
+/*      
+    ---------   FÖR ATT SKAPA ETT NYTT TABLE    ------------
+
+db.run( 
+        `CREATE TABLE users(username, password, id)`
+        ); */
+
+/*     
+    ---------   FÖR ATT LÄGGA IN NY CONTENT I USERS   ------------
+
+const sql = `INSERT INTO users(username, password, id)
+                    VALUES (?, ?, ?)`; 
+
+    db.run(sql, [`jonna`, `123`, 1], (err) => {
+        if(err) return console.error(err.message);
+        console.log("a new row has been created")
+    })  */
+
+
+
+
+
+
+
+
+
+
+
+
+/* app.get("/orders", function(req, res, next) {
   res.send(orders);
 });
 
@@ -15,7 +112,7 @@ app.post('/register', (req, res) => {
   const pass = utils.hashPassword("123")
   const correct = utils.comparePassword("123", "$2b$08$hx3YTfswKozSR9nm0oKHKevIo3ftrPt2ca5dE2on0lOg2jk5s3i7K")
   res.send({pass, correct})
-})
+}) */
 
 
 /* function getId(){
